af8d013fd128ac9d616440a2f59ab9105b1b6604
Samara Prass dos Santos <samaras@ufcspa.edu.br>
2022-04-23 12:30:34 -0300

    contentdate fix commit

--- pages/modules/VMAT_drgs.py
+++ pages/modules/VMAT_drgs.py
@@ -195,8 +195,9 @@ def vmat_drgs():
 
                 # Date from test image
                 dcm_read = DicomImage(file_open_drgs)
-                date_dcm = dcm_read.metadata.AcquisitionDate
-                if date_dcm == None:
+                try:
+                    date_dcm = dcm_read.metadata.AcquisitionDate
+                except:
                     date_dcm = dcm_read.metadata.ContentDate
 
                 date_time_obj = datetime.strptime(date_dcm, '%Y%m%d')
@@ -380,8 +381,9 @@ def vmat_drgs():
                         for filename in filenames:
                             filepath = os.path.join(root, filename)
                             dcm_read = pydicom.dcmread(filepath)
-                            st.session_state['date_img'] = dcm_read.AcquisitionDate
-                            if st.session_state['date_img'] == None:
+                            try:
+                                st.session_state['date_img'] = dcm_read.AcquisitionDate
+                            except:
                                 st.session_state['date_img'] = dcm_read.ContentDate
                     shutil.rmtree(path)
 
